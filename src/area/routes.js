75982e7ee1869d71fad1e821f7a32ec5a2b1de16
jgarcia691 <jagarciar.info@gmail.com>
2026-04-23 13:30:26 -0400

    ajuste de rutas area

--- src/area/routes.js
+++ src/area/routes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const areaController = require('./controller');
 const { verifyToken } = require('../middleware/auth');
 
-// Proteger todas las rutas de áreas usando el middleware verifyToken
+// Ruta pública para obtener las áreas (necesaria para el registro)
+router.get('/listar/', areaController.getAreas);
+
+// Proteger el resto de las rutas de áreas usando el middleware verifyToken
 router.use(verifyToken);
 
-router.post('/', areaController.createArea);
-router.get('/', areaController.getAreas);
-router.get('/:id', areaController.getAreaById);
-router.put('/:id', areaController.updateArea);
-router.delete('/:id', areaController.deleteArea);
+router.post('/crear/', areaController.createArea);
+router.get('/listar/:id', areaController.getAreaById);
+router.put('/actualizar/:id', areaController.updateArea);
+router.delete('/eliminar/:id', areaController.deleteArea);
 
 module.exports = router;
