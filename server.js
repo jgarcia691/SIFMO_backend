0a0116ce33f434a7267f90d327a530c1f8ae43da
jgarcia691 <jagarciar.info@gmail.com>
2026-04-24 13:37:12 -0400

    change

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.send('API SISTEMA DE INCIDENCIAS funcionando');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
